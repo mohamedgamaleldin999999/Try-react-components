500734510378f0aad06aefb251828e9fcfb005b3
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-10 20:47:43 +0300

    export hero

--- src/modules/hero.js
+++ src/modules/hero.js
@@ -1,3 +1,30 @@
 function HeroDetails(){
-  return (//html goes here)
+  return (
+    <section>
+        <div>
+          <span>Frontend Developer</span>
+          <span>Technical Writer</span>
+        </div>
+        <h1> Emmanuel F. Kumah.</h1>
+        <h2>I build things for the web.</h2>
+        <p>
+          I help businesses increase customer engagement and retention by
+          combining technology and design to create inviting, easy-to-use web
+          applications
+        </p>
+
+        <div>
+          <button>View portfolio</button>
+
+          <a
+            href="https://drive.google.com/file/d/1vfw-TMv9Wsg6zpPI454PnvH6Azn2bgOw/view?usp=sharing"
+            download
+          >
+            Download CV
+          </a>
+        </div>
+      </section>
+  )
 }
+
+export default HeroDetails
